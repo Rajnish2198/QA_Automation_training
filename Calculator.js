b8dd7ef587c2f698858f86fe65b63e5cd0af002a
Nidhi Bhardwaj <nbhardwaj@Ingenuity.local>
2026-07-07 21:37:32 +0530

    MyFirstCodeofCalculator

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+function calculator(a, b) {
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a%b);
+    console.log(a!=b);
+    console.log(a==b);
+    console.log(a===b);
+    console.log(a&&b==10);
+    console.log(a>=b);
+    console.log(a<=b);
+    
+
+
+}
+calculator(2, 5);
+console.log();
+console.log();
+calculator(10, 10);
---
+++ CalulatorwithIfElse.js
@@ -0,0 +1,28 @@
+let a=155;
+let b=10;
+
+if(a+b==100) {
+    console.log("a+b is equal to 100");
+
+}
+else if(a-b==5){
+    console.log("a-b is equal to 5");
+
+}
+else if(a*b==150){
+    console.log("a*b is equal to 150");
+
+}
+
+else if(a/b==1){
+    console.log("a/b is equal to 1");
+}
+
+else if(a%b==0){
+    console.log("a%b is equal to 0");
+}
+else {
+    console.log("a & b is matching any condition");
+
+}
+    
