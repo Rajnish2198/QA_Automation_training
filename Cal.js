76c97a34a6752b98b85fd1baceb7971915c69a8c
Rajeshwar <rajeshwarg143@gmail.com>
2026-07-08 23:12:45 +0530

    Calculator Program

---
+++ Cal.js
@@ -0,0 +1,20 @@
+console.log("Welcome to the Simple Calculator");
+
+function calculator(a, b, action) {
+    if (action === "add"){
+        return a+b;
+    } else if (action === "sub"){
+        return a-b;
+    } else if (action === "mul"){
+        return a*b;
+    } else if (action === "div"){
+        return a/b;
+    } else if ( action === "reminder"){
+        return a%b;
+    } else {
+        console.log("Invalid Action")
+    }
+}
+
+let output = calculator(20, 10, "div");
+console.log("Your Output is " + output);
