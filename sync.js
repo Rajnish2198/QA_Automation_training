a181f7e103f7239455a5ea559049a3acd511b7c3
Mahima Jain <majain@Ingenuity.local>
2026-07-20 23:54:17 +0530

    Synchronous code Example

---
+++ sync.js
@@ -0,0 +1,14 @@
+function sync()
+{
+  console.log("calculation started");
+
+  for(let i=1;i<=5;i++)
+  {
+    console.log(i);
+  }
+  
+}
+
+console.log("Calculation Started");
+sync();
+console.log("Calculation End");
