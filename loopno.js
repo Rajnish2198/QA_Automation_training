901354953ff0b7894f9de0668fa0ccf4647e5408
chandergit <chander@ingenuitygaming.com>
2026-07-27 14:31:30 +0530

    Uncommented code to print numbers and table of 7

--- loopno.js
+++ loopno.js
@@ -1,25 +1,25 @@
-// console.log("\nprint numbers from 1 to 20");
-// for (let i=1;i<=20;i++){
-//     console.log(i);
-// }
-// console.log("\nprint numbers from 20 to 1");
-// for (let i=20;i>=1;i--){
-//     console.log(i);
-// }
-// console.log("\nprint all even numbers from 1 to 50");
-// for (let i=2;i<=50;i+=2) {
-//     console.log(i);
-// }
-// console.log("\nprint all odd numbers from 1 to 49");
-// for (let i=1;i<=50;i+=2) {
-//     console.log(i);
-// }
-// console.log("\nprint the table of 7");
+console.log("\nprint numbers from 1 to 20");
+for (let i=1;i<=20;i++){
+    console.log(i);
+}
+console.log("\nprint numbers from 20 to 1");
+for (let i=20;i>=1;i--){
+    console.log(i);
+}
+console.log("\nprint all even numbers from 1 to 50");
+for (let i=2;i<=50;i+=2) {
+    console.log(i);
+}
+console.log("\nprint all odd numbers from 1 to 49");
+for (let i=1;i<=50;i+=2) {
+    console.log(i);
+}
+console.log("\nprint the table of 7");
 
-// for (let i=1;i<=10;i++)
-// {
-//     console.log(`7x${i}=${7*i}`);
-// }
+for (let i=1;i<=10;i++)
+{
+    console.log(`7x${i}=${7*i}`);
+}
 
 console.log("\nprint the sum of numbers from 1 to 10");
 function sum(){
